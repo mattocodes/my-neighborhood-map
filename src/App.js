0e62598ca1c4515b116076f322421e753d9250a2
trmatt <39041501+trmatt@users.noreply.github.com>
2018-11-25 00:59:37 -0600

    Define initMap, createScript, & insertScript functions

--- src/App.js
+++ src/App.js
@@ -2,10 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  initMap() {
+    let mesquite = {lat: 32.7668, lng: -96.5992};
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: mesquite,
+      zoom: 12
+    });
+  }
+
+  /**
+   * Gets the first script element on page, 
+   * creates a script element and inserts it
+   * the first script element on page
+   */
+  insertScript() {
+    let script = this.createScript();
+    let firstScriptElem = document.getElementsByTagName("script")[0];
+    firstScriptElem.parentNode.insertBefore(script, firstScriptElem);
+
+  }
+
+  /**
+   * Creates script tap
+   */
+  createScript() {
+    let script = window.document.createElement("script");
+    const API_KEY = "AIzaSyBs819gHKuu3HoieR6JWmgsKPCiKWFxF7w";
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    return script;
+  } 
+
   render() {
+    this.insertScript();
+    
     return (
-      <div className="App">
-        
+      <div className="App" id="map">
+
       </div>
     );
   }
